d81376df4331b42d389af7b50a701c42c9d1dc60
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-12 20:27:23 -0300

    add sql script to create and drop database

---
+++ backend/src/migrations/init.sql
@@ -0,0 +1,8 @@
+-- Connect to a different database
+\connect postgres
+
+-- Drop the 'hubla' database if it exists
+DROP DATABASE IF EXISTS hubla;
+
+-- Create a new 'hubla' database
+CREATE DATABASE hubla;
