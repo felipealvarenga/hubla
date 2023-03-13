12aafd75cdfaf669319dd8bbcf0ba602372358d9
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 14:25:37 -0300

    add swagger for backend docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
-import { APP_PIPE, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Swagger configuration
+  const options = new DocumentBuilder()
+    .setTitle('Hubla API')
+    .setDescription('Hubla api documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   app.enableCors();
   await app.listen(3333);
 }
