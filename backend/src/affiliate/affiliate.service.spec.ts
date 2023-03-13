7a8454bb86bc06a1e524ed677bd6e1f57203fa9b
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 19:41:54 -0300

    change affiliate service unit test to use same pattern

--- backend/src/affiliate/affiliate.service.spec.ts
+++ backend/src/affiliate/affiliate.service.spec.ts
@@ -1,21 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { AffiliateService } from './affiliate.service';
 import { Affiliate } from './entities/affiliate.entity';
+import { Sale } from '../sale/entities/sale.entity';
+import { Commission } from '../commission/entities/commission.entity';
+import { AffiliateService } from './affiliate.service';
 import { CreateAffiliateDto } from './dto/create-affiliate.dto';
-import { Creator } from '../creator/entities/creator.entity';
 
 describe('AffiliateService', () => {
-  let service: AffiliateService;
-  let repository: Repository<Affiliate>;
+  let affiliateService: AffiliateService;
+  let affiliateRepositoryMock: Record<string, jest.Mock>;
+  let saleRepositoryMock: Record<string, jest.Mock>;
+  let commissionRepositoryMock: Record<string, jest.Mock>;
+
+  const affiliate = new Affiliate();
+  affiliate.id = 1;
+  affiliate.name = 'John Doe';
 
   beforeEach(async () => {
-    const mockRepository = {
-      findOneBy: jest.fn(),
+    affiliateRepositoryMock = {
       save: jest.fn(),
+      findOneBy: jest.fn(),
       find: jest.fn(),
       create: jest.fn(),
+    };
+    saleRepositoryMock = {
+      createQueryBuilder: jest.fn(() => ({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue([]),
+      })),
+    };
+    commissionRepositoryMock = {
+      createQueryBuilder: jest.fn(() => ({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue([]),
+      })),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -23,15 +44,20 @@ describe('AffiliateService', () => {
         AffiliateService,
         {
           provide: getRepositoryToken(Affiliate),
-          useValue: mockRepository,
+          useValue: affiliateRepositoryMock,
+        },
+        {
+          provide: getRepositoryToken(Sale),
+          useValue: saleRepositoryMock,
+        },
+        {
+          provide: getRepositoryToken(Commission),
+          useValue: commissionRepositoryMock,
         },
       ],
     }).compile();
 
-    service = module.get<AffiliateService>(AffiliateService);
-    repository = module.get<Repository<Affiliate>>(
-      getRepositoryToken(Affiliate),
-    );
+    affiliateService = module.get<AffiliateService>(AffiliateService);
   });
 
   afterEach(() => {
@@ -41,139 +67,95 @@ describe('AffiliateService', () => {
   describe('save', () => {
     it('should save a new affiliate', async () => {
       const createAffiliateDto: CreateAffiliateDto = {
-        name: 'Test Affiliate',
         creator_id: 1,
+        name: 'Jane Doe',
       };
-      const affiliate = new Affiliate();
-      affiliate.name = createAffiliateDto.name;
+      affiliateRepositoryMock.findOneBy.mockResolvedValueOnce(null);
+      affiliateRepositoryMock.save.mockResolvedValueOnce(affiliate);
+      affiliateRepositoryMock.create.mockReturnValue(affiliate);
 
-      const creator = new Creator();
-      creator.id = 1;
-      creator.name = 'Test Creator';
+      const result = await affiliateService.save(createAffiliateDto);
 
-      affiliate.creator = creator;
-
-      const saveSpy = jest
-        .spyOn(repository, 'save')
-        .mockResolvedValueOnce(affiliate);
-
-      jest.spyOn(repository, 'create').mockReturnValue(affiliate);
-      const result = await service.save(createAffiliateDto);
-
-      expect(saveSpy).toHaveBeenCalledWith(affiliate);
-      expect(result).toEqual(affiliate);
+      expect(affiliateRepositoryMock.save).toHaveBeenCalledWith(affiliate);
+      expect(result).toStrictEqual(affiliate);
     });
 
-    it('should return an existing affiliate if it exists', async () => {
+    it('should return an existing affiliate', async () => {
       const createAffiliateDto: CreateAffiliateDto = {
-        name: 'Test Affiliate',
+        name: affiliate.name,
         creator_id: 1,
       };
-      const existingAffiliate = new Affiliate();
-      existingAffiliate.id = 1;
-      existingAffiliate.name = createAffiliateDto.name;
-      const findOneBySpy = jest
-        .spyOn(repository, 'findOneBy')
-        .mockResolvedValueOnce(existingAffiliate);
+      affiliateRepositoryMock.findOneBy.mockResolvedValueOnce(affiliate);
 
-      const result = await service.save(createAffiliateDto);
+      const result = await affiliateService.save(createAffiliateDto);
 
-      expect(findOneBySpy).toHaveBeenCalledWith({
+      expect(affiliateRepositoryMock.findOneBy).toHaveBeenCalledWith({
         name: createAffiliateDto.name,
       });
-      expect(result).toEqual(existingAffiliate);
+      expect(affiliateRepositoryMock.save).not.toHaveBeenCalled();
+      expect(result).toBe(affiliate);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of affiliates', async () => {
+      affiliateRepositoryMock.find.mockResolvedValueOnce([affiliate]);
+
+      const result = await affiliateService.findAll();
+
+      expect(affiliateRepositoryMock.find).toHaveBeenCalled();
+      expect(result).toEqual([affiliate]);
     });
   });
 
   describe('findByName', () => {
-    it('should return an affiliate by name', async () => {
-      const affiliate = new Affiliate();
-      affiliate.id = 1;
-      affiliate.name = 'Test Affiliate';
-      const findOneBySpy = jest
-        .spyOn(repository, 'findOneBy')
-        .mockResolvedValueOnce(affiliate);
+    it('should return an affiliate', async () => {
+      affiliateRepositoryMock.findOneBy.mockResolvedValueOnce(affiliate);
 
-      const result = await service.findByName(affiliate.name);
+      const result = await affiliateService.findByName(affiliate.name);
 
-      expect(findOneBySpy).toHaveBeenCalledWith({ name: affiliate.name });
-      expect(result).toEqual(affiliate);
+      expect(affiliateRepositoryMock.findOneBy).toHaveBeenCalledWith({
+        name: affiliate.name,
+      });
+      expect(result).toBe(affiliate);
     });
 
-    it('should return null if the affiliate is not found', async () => {
-      const findOneBySpy = jest
-        .spyOn(repository, 'findOneBy')
-        .mockResolvedValueOnce(null);
+    it('should return null if affiliate is not found', async () => {
+      affiliateRepositoryMock.findOneBy.mockResolvedValueOnce(null);
 
-      const result = await service.findByName('Non-existing Affiliate');
+      const result = await affiliateService.findByName('Unknown');
 
-      expect(findOneBySpy).toHaveBeenCalledWith({
-        name: 'Non-existing Affiliate',
+      expect(affiliateRepositoryMock.findOneBy).toHaveBeenCalledWith({
+        name: 'Unknown',
       });
       expect(result).toBeNull();
     });
   });
 
-  describe('findAll', () => {
-    it('should return all affiliates', async () => {
-      const affiliates = [
-        { id: 1, name: 'Affiliate 1' },
-        { id: 2, name: 'Affiliate 2' },
-      ];
-      const findSpy = jest
-        .spyOn(repository, 'find')
-        .mockResolvedValueOnce(
-          affiliates.map((a) => Object.assign(new Affiliate(), a)),
-        );
-
-      const result = await service.findAll();
-
-      expect(findSpy).toHaveBeenCalled();
-      expect(result).toEqual(affiliates);
-    });
-  });
-
   describe('getBalance', () => {
-    // it('should return the balance for a specific affiliate', async () => {
-    //   const affiliate = new Affiliate();
-    //   affiliate.id = 1;
-    //   affiliate.name = 'Test Affiliate';
+    it('should return the balance of an affiliate', async () => {
+      const commission1 = new Commission();
+      commission1.amount = 100;
+      const commission2 = new Commission();
+      commission2.amount = 50;
 
-    //   const comissions = new Array<Commission>();
+      affiliateRepositoryMock.find.mockResolvedValueOnce([
+        { ...affiliate, commissions: [commission1, commission2] },
+      ]);
 
-    //   comissions.push({ id: 1, amount: 10 });
-    //   affiliate.commissions = [
-    //     { id: 1, amount: 10 },
-    //     { id: 2, amount: 20 },
-    //   ];
-    //   const findSpy = jest
-    //     .spyOn(repository, 'find')
-    //     .mockResolvedValueOnce([affiliate]);
-    //   const result = await service.getBalance(affiliate.id);
+      const result = await affiliateService.getBalance(affiliate.id);
 
-    //   expect(findSpy).toHaveBeenCalledWith({
-    //     where: { id: affiliate.id },
-    //     relations: { commissions: true },
-    //   });
-    //   expect(result).toEqual([
-    //     {
-    //       id: affiliate.id,
-    //       name: affiliate.name,
-    //       balance: 30,
-    //     },
-    //   ]);
-    // });
-
-    it('should return an empty array if the affiliate is not found', async () => {
-      const findSpy = jest.spyOn(repository, 'find').mockResolvedValueOnce([]);
-
-      const result = await service.getBalance(1);
-
-      expect(findSpy).toHaveBeenCalledWith({
-        where: { id: 1 },
+      expect(affiliateRepositoryMock.find).toHaveBeenCalledWith({
+        where: { id: affiliate.id },
         relations: { commissions: true },
       });
-      expect(result).toEqual([]);
+      expect(result).toEqual([
+        {
+          id: affiliate.id,
+          name: affiliate.name,
+          balance: 150,
+        },
+      ]);
     });
   });
 });
