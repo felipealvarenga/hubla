83fed8817948444928933f3b5403a2e12c7a23f0
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 14:36:17 -0300

    Create backend doc for creator controller and dto

--- backend/src/creator/creator.controller.ts
+++ backend/src/creator/creator.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { CreatorService } from './creator.service';
 
 @Controller('creator')
@@ -6,11 +7,23 @@ export class CreatorController {
   constructor(private readonly creatorService: CreatorService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all creators' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all creators.',
+  })
+  @Get()
   findAll() {
     return this.creatorService.findAll();
   }
   @Get(':id/balance')
-  async getBalance(@Param('id') id: string): Promise<{ balance: number }> {
+  @ApiOperation({ summary: 'Get creator balance by ID' })
+  @ApiParam({ name: 'id', description: 'Creator ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return creator balance by ID.',
+  })
+  async getBalance(@Param('id') id: string) {
     return this.creatorService.getBalance(+id);
   }
 }
