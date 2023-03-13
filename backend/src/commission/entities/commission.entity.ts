53a54c51bdf76a529a41b39c09c8b0dcd2a9acac
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-12 22:30:48 -0300

    Change commission entity mapping with all relations

--- backend/src/commission/entities/commission.entity.ts
+++ backend/src/commission/entities/commission.entity.ts
@@ -1,1 +1,36 @@
-export class Commission {}
+import { Affiliate } from '../../affiliate/entities/affiliate.entity';
+import { Creator } from '../../creator/entities/creator.entity';
+import { Product } from '../../product/entities/product.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('commission')
+export class Commission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  date: Date;
+
+  @ManyToOne(() => Affiliate, (affiliate) => affiliate.commissions, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'affiliate_id' })
+  affiliate?: Affiliate;
+
+  @ManyToOne(() => Creator, { nullable: false })
+  @JoinColumn({ name: 'creator_id' })
+  creator: Creator;
+
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+}
