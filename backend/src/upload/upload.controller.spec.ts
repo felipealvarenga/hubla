f8fdcd40aa08ccc6889645637d3dda7cac446e1c
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 18:02:29 -0300

    create upload controller unit test

--- backend/src/upload/upload.controller.spec.ts
+++ backend/src/upload/upload.controller.spec.ts
@@ -1,20 +1,74 @@
+import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 describe('UploadController', () => {
-  let controller: UploadController;
+  let uploadController: UploadController;
+
+  const mockUploadService = {
+    parseAndSave: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UploadController],
-      providers: [UploadService],
+      providers: [
+        {
+          provide: UploadService,
+          useValue: mockUploadService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<UploadController>(UploadController);
+    uploadController = module.get<UploadController>(UploadController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('uploadFile', () => {
+    it('should return an error message if no file is uploaded', async () => {
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const file = undefined;
+
+      await uploadController.uploadFile(response, file);
+
+      expect(response.status).toHaveBeenCalledWith(HttpStatus.BAD_REQUEST);
+      expect(response.json).toHaveBeenCalledWith({
+        message: 'No file uploaded',
+      });
+    });
+
+    it('should call parseAndSave method of uploadService and return uploaded file', async () => {
+      const response = {
+        send: jest.fn(),
+      };
+      const file = {
+        fieldname: 'file',
+        originalname: 'test-file.txt',
+        encoding: '7bit',
+        mimetype: 'text/plain',
+        size: 4,
+        buffer: Buffer.from('test'),
+        stream: null,
+        destination: null,
+        filename: null,
+        path: null,
+      };
+
+      jest
+        .spyOn(mockUploadService, 'parseAndSave')
+        .mockResolvedValueOnce(undefined);
+
+      await uploadController.uploadFile(response, file);
+
+      expect(mockUploadService.parseAndSave).toHaveBeenCalledWith(file);
+      expect(response.send).toHaveBeenCalledWith(file);
+    });
   });
 });
