0be058ede8ea4e7467e634f5948f4163764408e0
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-12 22:31:13 -0300

    Add create commission service

--- backend/src/commission/commission.service.ts
+++ backend/src/commission/commission.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCommissionDto } from './dto/create-commission.dto';
+import { Commission } from './entities/commission.entity';
 
 @Injectable()
 export class CommissionService {
-  create(createCommissionDto: CreateCommissionDto) {
-    return 'This action adds a new commission';
+  constructor(
+    @InjectRepository(Commission)
+    private commissionRepository: Repository<Commission>,
+  ) {}
+  async create(createCommissionDto: CreateCommissionDto) {
+    const { amount, date, product_id, affiliate_id, creator_id } =
+      createCommissionDto;
+
+    const commission = this.commissionRepository.create({
+      amount,
+      date,
+      product: { id: product_id },
+      creator: { id: creator_id },
+      affiliate: affiliate_id ? { id: affiliate_id } : undefined,
+    });
+
+    return this.commissionRepository.save(commission);
   }
 }
