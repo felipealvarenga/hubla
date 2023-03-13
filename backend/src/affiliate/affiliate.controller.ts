a7c0d79d8d149828d7922d3b1b9d9d24d0e02344
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 14:35:13 -0300

    Create Affiliate backend docs

--- backend/src/affiliate/affiliate.controller.ts
+++ backend/src/affiliate/affiliate.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 import { AffiliateService } from './affiliate.service';
 
 @Controller('affiliate')
+@ApiTags('Affiliate')
 export class AffiliateController {
   constructor(private readonly affiliateService: AffiliateService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all affiliates' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all affiliates.',
+  })
   findAll() {
     return this.affiliateService.findAll();
   }
+
   @Get(':id/balance')
-  async getBalance(
-    @Param('id') id: string,
-  ): Promise<{ id: number; name: string; balance: number }> {
+  @ApiOperation({ summary: 'Get affiliate balance by ID' })
+  @ApiParam({ name: 'id', description: 'Affiliate ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return affiliate balance by ID.',
+  })
+  async getBalance(@Param('id') id: string) {
     const [result] = await this.affiliateService.getBalance(+id);
     return result;
   }
