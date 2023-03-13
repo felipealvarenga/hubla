eab2c78041bcd2a797acc0a5588a7fe98fe9512f
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 18:01:53 -0300

    create product controller unit test

--- backend/src/product/product.controller.spec.ts
+++ backend/src/product/product.controller.spec.ts
@@ -5,16 +5,92 @@ import { ProductService } from './product.service';
 describe('ProductController', () => {
   let controller: ProductController;
 
+  const mockProductService = {
+    findAllWithSalesAndCommissions: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [ProductService],
+      providers: [
+        {
+          provide: ProductService,
+          useValue: mockProductService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('getAllProductsTransactions', () => {
+    it('should return an array of products with sales and commissions data', async () => {
+      const result = [
+        {
+          id: 1,
+          name: 'CURSO',
+          sales: [
+            {
+              id: 1,
+              date: '2022-03-01T05:09:54.000Z',
+              amount: 12750,
+              product: {
+                id: 1,
+                name: 'CURSO',
+              },
+              creator: {
+                id: 1,
+                name: 'JOSE CARLOS',
+              },
+              affiliate: null,
+            },
+          ],
+          commissions: [
+            {
+              id: 1,
+              amount: -4500,
+              date: '2022-01-16T17:13:54.000Z',
+              product: {
+                id: 1,
+                name: 'CURSO',
+              },
+              creator: {
+                id: 37,
+                name: 'JOSE CARLOS',
+              },
+              affiliate: null,
+            },
+          ],
+        },
+      ];
+      mockProductService.findAllWithSalesAndCommissions.mockResolvedValue(
+        result,
+      );
+
+      const response = await controller.getAllProductsTransactions();
+
+      expect(response).toBe(result);
+      expect(
+        mockProductService.findAllWithSalesAndCommissions,
+      ).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an error if findAllWithSalesAndCommissions fails', async () => {
+      const error = new Error('findAllWithSalesAndCommissions failed');
+      mockProductService.findAllWithSalesAndCommissions.mockRejectedValue(
+        error,
+      );
+
+      await expect(
+        controller.getAllProductsTransactions(),
+      ).rejects.toThrowError(error);
+      expect(
+        mockProductService.findAllWithSalesAndCommissions,
+      ).toHaveBeenCalledTimes(1);
+    });
   });
 });
