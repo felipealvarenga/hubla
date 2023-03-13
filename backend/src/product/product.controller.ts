bbdb7085bc11b7a6dcda0fdc6287e45936b69376
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 14:37:52 -0300

    Create backend doc for product controller and dto

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @Get('/transactions')
+  @ApiOperation({ summary: 'Get all products transactions' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all products transactions.',
+  })
   @Get('/transactions')
   getAllProductsTransactions() {
     return this.productService.findAllWithSalesAndCommissions();
