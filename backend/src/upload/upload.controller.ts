f3bdd04257003a237c953ffa1a394d8af0d68835
felipealvarenga <felipealvarengarc@gmail.com>
2023-03-13 14:41:40 -0300

    Create backend doc for upload controller

--- backend/src/upload/upload.controller.ts
+++ backend/src/upload/upload.controller.ts
@@ -8,13 +8,28 @@ import {
   Res,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBadRequestResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UploadService } from './upload.service';
 
 @Controller('upload')
+@ApiTags('Upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
   @Post()
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: 'Upload a file of product transactions' })
+  @ApiBadRequestResponse({
+    description: 'There is an error in the uploaded file',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Upload transactions file',
+  })
   async uploadFile(@Res() res, @UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new HttpException(
